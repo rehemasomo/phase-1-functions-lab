344c8c4b23a25353cc2bf7e6d217e22e72ad7598
rehemasomo <rehemasomo30@gmail.com>
2024-03-22 21:43:06 +0300

    completed functions lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+    return Math.abs(location - 42)
+}
+function distanceFromHqInFeet(location) {
+    const blocks = distanceFromHqInBlocks(location);
+    return blocks * 264
+}
+
+function distanceTravelledInFeet(start,destination) {
+    return Math.abs(destination - start) * 264;
+}
+
+function calculatesFarePrice(start,destination) {
+    const distance = distanceTravelledInFeet(start,destination);
+    let fare = 0;
+
+    if (distance <= 400) {
+        fare = 0;
+    } else if (distance > 400 && distance <= 2000) {
+        fare =(distance - 400) * 0.02;
+    } else if (distance > 2000 && distance < 2500) {
+        fare = 25;
+    } else {
+        return 'cannot travel that far';
+    }
+    return parseFloat(fare.toFixed(2));//Convert the fare to a number
+}
+module.exports={
+    distanceFromHqInBlocks,
+    distanceFromHqInFeet,
+    distanceTravelledInFeet,
+    calculatesFarePrice
+};
